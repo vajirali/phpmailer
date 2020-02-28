d336da195973aed3b91ab4cf1a8c92d69d504c47
Vajir Ali Asamadi <Vajir@SAITEC.local>
2020-02-28 12:08:11 +0530

    change msg

--- sendMail.php
+++ sendMail.php
@@ -34,7 +34,7 @@ try {
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Message has been sent...';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
